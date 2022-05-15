4300b6b2a7758a2d3cd3949944c0dc826ba0964a
Jason <jasonlucas443@gmail.com>
2022-05-15 10:44:10 -0300

    list games visual page

---
+++ src/assets/styled/ListGames/StyledListGames.js
@@ -0,0 +1,95 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+    box-sizing: border-box;
+    padding: 10px;
+    width: 100%;
+    height: 70px;
+    background-color: rgb(11, 13, 18);
+`;
+
+export const RowSearch = styled.div`
+    box-sizing: border-box;
+    min-width: 200px;
+    max-width: 500px;
+    height: auto;
+    margin: auto;
+    margin-top: 10px;
+`; 
+
+export const SearchField = styled.input`
+    box-sizing: border-box;
+    border: none;
+    width: 100%;
+    height: auto;
+    padding: 7px;
+    border-radius: 5px;
+`;
+
+export const ContainerGames = styled.div`
+    box-sizing: border-box;
+    width: 90%;
+    height: auto;
+    margin: auto;
+    padding: 10px;
+`;
+
+export const SingleGame = styled.div`
+    box-sizing: border-box;
+    width: 170px;
+    height: 250px;
+    float: left;
+    margin-right: 20px;
+    margin-bottom: 20px;
+    cursor: pointer;
+`;
+
+export const GameThumbnail = styled.div`
+    height: 70%;
+    width: 100%;
+    box-sizing: border-box;
+`;
+
+export const Img = styled.img`
+    height: 100%;
+    width: 100%;
+`;
+
+export const RowGameData = styled.div`
+    box-sizing: border-box;
+    width: 100%;
+    height: 30%;
+`;
+
+export const GameData = styled.p`
+    box-sizing: border-box;
+    padding: 10px 0px 10px 0px;
+    word-wrap: break-word;
+    font-size: 16px;
+    color: white;
+    ${(type) => type === 'price' ?  'font-weight:bold;' : '' }
+    font-family: 'Raleway', sans-serif;
+`;
+
+export const FooterPage = styled.footer`
+    box-sizing: border-box;
+    padding: 10px;
+    width: 100%;
+    height: 50px;
+    background-color: rgb(11, 13, 18);
+    justify-content: space-between;
+    display: flex;
+    position: fixed;
+    bottom: 0;
+`;
+
+export const FooterIcons = styled.p`
+    width: 50px;
+    box-sizing: border-box;
+    padding: 5px;
+    color: white;
+    font-size: 23px;
+    text-align: center;
+    font-weight: bold;
+    cursor: pointer;
+`;
