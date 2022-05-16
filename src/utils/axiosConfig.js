7c005493132b87a16d3c644e436c2ca21e66aaa9
Danilo <danilo_srr@hotmail.com>
2022-05-16 17:14:12 -0300

    finished

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-const axiosConfig = axios.create({baseURL: 'https://drizzard.herokuapp.com/'}); //https://drizzard.herokuapp.com
+const axiosConfig = axios.create({baseURL: 'http://localhost:5000'}); //https://drizzard.herokuapp.com
 export default axiosConfig;
